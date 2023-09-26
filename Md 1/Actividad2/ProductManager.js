44c2e59a3fbfd2334884b698cc602f96639611fc
Nehuen Franco <teonehuen409@gmail.com>
2023-09-26 19:25:17 -0300

    Actividad2Corrección

--- Md 1/Actividad2/ProductManager.js
+++ Md 1/Actividad2/ProductManager.js
@@ -1,37 +1,41 @@
 const fs = require('fs');
 const fsp = require('fs').promises;
 
-const readDocument = async (path) => {
-    try {
-        if (fs.existsSync(path)) {
-            let content = await fsp.readFile(path, 'utf-8');
-            if (!content) {
-                return [];
-            }
-            return JSON.parse(content);
-        } else {
-            console.error("no hay archivo al cual leer");
-            return [];
-        }
-    } catch (error) {
-        console.error(`Error reading document: ${error.message}`);
-    }
-    return [];
-};
 class ProductManager{
     constructor(path) {
         this.path = path;
         this.nextId = 1;
         this.init();
+        this.readDocument = async () => {
+            try {
+                if (fs.existsSync(this.path)) {
+                    let content = await fsp.readFile(this.path, 'utf-8');
+                    if (!content) {
+                        return [];
+                    }
+                    return JSON.parse(content);
+                } else {
+                    console.error("no hay archivo al cual leer");
+                    return [];
+                }
+            } catch (error) {
+                console.error(`Error reading document: ${error.message}`);
+                return [];
+            }
+        };
     }
     async init() {
         if (!fs.existsSync(this.path)) {
             await fsp.writeFile(this.path, JSON.stringify([]));
         }
     }
+    
 
     async addProduct(product) {
-        let arrayProducts = await readDocument(this.path);
+        let arrayProducts = await this.readDocument();
+        const maxId = Math.max(...arrayProducts.map(p => p.id), 0);
+        this.nextId = maxId + 1;
+
         if (arrayProducts.some(existingProduct => existingProduct.code === product.code)) {
             console.error("The product is already added");
             return;
@@ -56,11 +60,11 @@ class ProductManager{
     }
 
     async getProducts() {
-        let products = await readDocument(this.path);
+        let products = await this.readDocument();
         return products;
     }
     async getProductById(id) {
-        let arrayProducts = await readDocument(this.path);
+        let arrayProducts = await this.readDocument();
         const product = arrayProducts.find(product => product.id === id);
         
         if (!product) {
@@ -71,7 +75,7 @@ class ProductManager{
         return product;
     }
     async updateProduct(id, updatedProduct) {
-        let arrayProducts = await readDocument(this.path);
+        let arrayProducts = await this.readDocument();
         const index = arrayProducts.findIndex(product => product.id === id);
 
         if (index === -1) {
@@ -84,7 +88,7 @@ class ProductManager{
 
     }
     async deleteProduct(id) {
-        let arrayProducts = await readDocument(this.path);
+        let arrayProducts = await this.readDocument();
         const index = arrayProducts.findIndex(product => product.id === id);
 
         if (index === -1) {
@@ -97,40 +101,54 @@ class ProductManager{
     }
 }
 
-const products_prueba = new ProductManager("./Products1.json");
-console.log(products_prueba.getProducts())
-let newProduct = {
-    title: "producto prueba",
-    description: "este es un producto prueba",
-    price: 200,
-    thumbnail: "sin imagen",
-    code: "abc123",
-    stock: 25,
-}
-let newProduct2 = {
-    title: "producto prueba",
-    description: "este es un producto prueba",
-    price: 200,
-    thumbnail: "sin imagen",
-    code: "abc1234",
-    stock: 25,
-}
-let newProduct3 = {
-    title: "producto prueba",
-    description: "este es un producto prueba",
-    price: 200,
-    thumbnail: "sin imagen",
-    code: "abc12345",
-    stock: 25,
-}
-products_prueba.addProduct(newProduct) 
-products_prueba.addProduct(newProduct2) 
-products_prueba.addProduct(newProduct3)
-console.log(products_prueba.getProducts())
-console.log(products_prueba.getProductById(2))
 
-products_prueba.updateProduct(2, { title: "updated title" });
-console.log(products_prueba.getProductById(2));
+const startAsync = async () => {
+    const products_prueba = new ProductManager("./Products1.json");
+    console.log(await products_prueba.getProducts());
+    
+    let newProduct = {
+        title: "producto prueba",
+        description: "este es un producto prueba",
+        price: 200,
+        thumbnail: "sin imagen",
+        code: "abc123",
+        stock: 25,
+    }
+    let newProduct2 = {
+        title: "producto prueba",
+        description: "este es un producto prueba",
+        price: 200,
+        thumbnail: "sin imagen",
+        code: "abc1234",
+        stock: 25,
+    }
+    let newProduct3 = {
+        title: "producto prueba",
+        description: "este es un producto prueba",
+        price: 200,
+        thumbnail: "sin imagen",
+        code: "abc12345",
+        stock: 25,
+    }
 
-products_prueba.deleteProduct(2);
-console.log(products_prueba.getProducts());
+    await products_prueba.addProduct(newProduct);
+    await products_prueba.addProduct(newProduct2);
+    await products_prueba.addProduct(newProduct3);
+    
+    console.log("productos:")
+    console.log(await products_prueba.getProducts());
+
+    console.log("producto por id:")
+    console.log(await products_prueba.getProductById(2));
+
+    console.log("updated product:")
+    await products_prueba.updateProduct(2, { title: "updated title" });
+    console.log(await products_prueba.getProductById(2));
+
+    console.log("delete product:")
+    await products_prueba.deleteProduct(2);
+    console.log(await products_prueba.getProducts());
+}
+
+startAsync();
+
